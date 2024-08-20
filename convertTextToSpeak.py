5972be4e934edf2fccd6a35d633ad26c245b545f
Ahmed ElSherif <us.ahmedelsherif@gmail.com>
2024-08-20 13:26:40 +0300

    Add files via upload

---
+++ convertTextToSpeak.py
@@ -0,0 +1,10 @@
+#pip install playsound --break-system-packages
+
+from gtts import gTTS
+import playsound
+import os
+
+text = gTTS(text='oh my gotto what are you doing youssef',lang="en",slow=False)
+text.save("sound.mp4")
+playsound.playsound("sound.mp4",True)
+os.remove("sound.mp4")
