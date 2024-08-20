9ad84c58c6f2048451b34216844cbd992e1509f5
Ahmed ElSherif <us.ahmedelsherif@gmail.com>
2024-08-20 13:30:39 +0300

    Update convertTextToSpeak.py

--- convertTextToSpeak.py
+++ convertTextToSpeak.py
@@ -4,7 +4,7 @@ from gtts import gTTS
 import playsound
 import os
 
-text = gTTS(text='oh my gotto what are you doing youssef',lang="en",slow=False)
+text = gTTS(text='oh my gotto what are you doing man',lang="en",slow=False)
 text.save("sound.mp4")
 playsound.playsound("sound.mp4",True)
 os.remove("sound.mp4")
